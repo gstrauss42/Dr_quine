e2b48156416d3065f36390e1dad8c927b98539c9
Gabriel <gstrauss18@gmail.com>
2019-09-06 20:29:11 +0200

    stuff

--- Grace.c
+++ Grace.c
@@ -1,6 +1,7 @@
-#include <fcntl.h>
+#include <fcntl.h>/*the specified comment*/
 #include <stdio.h>
 
-#define data "#include <fcntl.h>%c#include <stdio.h>%c%c#define data %c%s%c%c#define MAIN int main(void){int fd = open(%cGrace_kid.c%c, O_RDWR);dprintf(1, data);}%cMAIN"
-#define MAIN int main(void){int fd = open("Grace_kid.c", O_RDWR);dprintf(1, data, 10, 10, 10, 10, data, 10, 10, 34, 34, 10);}
+#define waste 10
+#define data "#include <fcntl.h>/*the specified comment*/%c#include <stdio.h>%c%c#define waste 10%c#define data %c%s%c%c#define MAIN int main(void){int fd = open(%cGrace_kid.c%c, O_RDWR); dprintf(fd, data, waste, waste, waste, waste, 34, data, 34, waste, 34, 34, waste, waste);}%cMAIN%c"
+#define MAIN int main(void){int fd = open("Grace_kid.c", O_RDWR); dprintf(fd, data, waste, waste, waste, waste, 34, data, 34, waste, 34, 34, waste, waste);}
 MAIN
