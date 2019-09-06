ab5b77de243b7a6239ea81954abb6de3fa8a8171
Gabriel <gstrauss18@gmail.com>
2019-09-06 15:41:41 +0200

    stuff

--- Grace.c
+++ Grace.c
@@ -1,6 +1,6 @@
 #include <fcntl.h>
-#include <unistd.h>
+#include <stdio.h>
 
-#define data int main(void){int fd = open("Grace_kid.c", O_RDWR); write(fd, "works", 5);}
-
-data
+#define data "#include <fcntl.h>%c#include <stdio.h>%c%c#define data %c%s%c%c#define MAIN int main(void){int fd = open(%cGrace_kid.c%c, O_RDWR);dprintf(1, data);}%cMAIN"
+#define MAIN int main(void){int fd = open("Grace_kid.c", O_RDWR);dprintf(1, data, 10, 10, 10, 10, data, 10, 10, 34, 34, 10);}
+MAIN
--- Grace_kid.c
+++ Grace_kid.c
@@ -1,1 +1,6 @@
-works
+#include <fcntl.h>
+#include <stdio.h>
+
+#define data 
+#define main int main(void){int fd = open(Grace_kid.c, O_RDWR); dprintf(fd, );}
+main
