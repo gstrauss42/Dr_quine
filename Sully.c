b6310e9f1c1ef81a8b710c1d7b260eccfbc1472f
Gabriel <gstrauss18@gmail.com>
2019-09-09 11:24:27 +0200

    Stuff

--- Sully.c
+++ Sully.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <fcntl.h>
 
-#define data "#include <stdio.h>%c#include <stdlib.h>%c#include <string.h>%c#include <fcntl.h>%c%c#define data %c%s%c%c%cint	main()%c{%c	char *buff = strdup(%cSully_X.c%c);%c	int i = %d;%c	buff[6] = i + 48;%c	FILE	*fd = fopen(buff, %cw+%c);%c	fprintf(fd, data, 10, 10, 10, 10, 10, 34, data, 34, 10, 10, 10, 10, 34, 34, 10, (i - 1), 10, 10, 34, 34, 10, 10, 34, (i - 1), 34, 10, 10, 10, 10, 34, (i - 1), 34, 10, 10, 10);	%c	char *tmp = strdup(%cgcc -o Sully_%d %c);%c	char *val = strdup(strcat(tmp, buff));%c	fclose(fd);%c	system (val);%c	system (%c./Sully_%d%c);%c	free(buff); free(tmp); free(val);%c}%c"
+#define data "#include <stdio.h>%c#include <stdlib.h>%c#include <string.h>%c#include <fcntl.h>%c%c#define data %c%s%c%c%cint	main()%c{%c	char *buff = strdup(%cSully_X.c%c);%c	int i = %d;%c	buff[6] = i + 48;%c	FILE	*fd = fopen(buff, %cw+%c);%c	fprintf(fd, data, 10, 10, 10, 10, 10, 34, data, 34, 10, 10, 10, 10, 34, 34, 10, (i - 1), 10, 10, 34, 34, 10, 10, 34, (i - 1), 34, 10, 10, 10, 34, 34, 10, 10, 34, 34, 10, 10, 10, 10, 10, 10, 10);%c	char *tmp = strdup(%cgcc -o Sully_%d %c);%c	char *val = strdup(strcat(tmp, buff));%c	fclose(fd);%c	char *qack = strdup(%cSully_X%c);%c	qack[6] = i + 48;%c	char *ttmp = strdup(%c./%c);%c	if (i == 0)%c		return (0);%c	system (val);%c	system (strcat(ttmp, qack));%c	free(buff); free(tmp); free(val);%c}%c"
 
 int	main()
 {
@@ -11,11 +11,16 @@ int	main()
 	int i = 5;
 	buff[6] = i + 48;
 	FILE	*fd = fopen(buff, "w+");
-	fprintf(fd, data, 10, 10, 10, 10, 10, 34, data, 34, 10, 10, 10, 10, 34, 34, 10, (i - 1), 10, 10, 34, 34, 10, 10, 34, (i - 1), 34, 10, 10, 10, 10, 34, (i - 1), 34, 10, 10, 10);
+	fprintf(fd, data, 10, 10, 10, 10, 10, 34, data, 34, 10, 10, 10, 10, 34, 34, 10, (i - 1), 10, 10, 34, 34, 10, 10, 34, (i - 1), 34, 10, 10, 10, 34, 34, 10, 10, 34, 34, 10, 10, 10, 10, 10, 10, 10);
 	char *tmp = strdup("gcc -o Sully_5 ");
 	char *val = strdup(strcat(tmp, buff));
 	fclose(fd);
+	char *qack = strdup("Sully_X");
+	qack[6] = i + 48;
+	char *ttmp = strdup("./");
+	if (i == 0)
+		return (0);
 	system (val);
-	system ("./Sully_5");
+	system (strcat(ttmp, qack));
 	free(buff); free(tmp); free(val);
 }
